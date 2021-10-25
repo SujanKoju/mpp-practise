9292bb34c2a213dad243e94c797c9afaaaa45dde
szn1995 <sujan.koju.sastra@gmail.com>
2021-10-24 21:11:18 -0500

    test for sum method done

---
+++ src/main/java/com/example/mpp/Mpp.java
@@ -0,0 +1,36 @@
+package com.example.mpp;
+
+import java.util.List;
+
+public class Mpp {
+
+    public static <T> void removeDuplicates(List<T> list) {
+        System.out.println("--- INITIAL LIST -->" + list);
+        if (list == null) {
+            System.out.println("NULL");
+        }
+        if (list.size() > 1) {
+
+            for (int i = 0; i < list.size(); i++) {
+                for (int j = i + 1; j < list.size(); j++) {
+                    if (list.get(i).equals(list.get(j))) {
+                        list.remove(j);
+                    }
+                }
+            }
+        }
+        System.out.println("--- FINAL LIST -->" + list);
+    }
+
+    public static boolean testForSum(int[] intArr, int testInt) {
+        if (intArr == null || intArr.length < 3) return false;
+        for (int i = 0; i < intArr.length - 2; i++) {
+            for (int j = i + 1; j < intArr.length - 1; j++) {
+                for (int k = j + 1; j < intArr.length; j++) {
+                    if (intArr[i] + intArr[j] + intArr[k] == testInt) return true;
+                }
+            }
+        }
+        return false;
+    }
+}
--- src/main/java/com/example/mpp/MppApplication.java
+++ src/main/java/com/example/mpp/MppApplication.java
@@ -11,17 +11,16 @@ public class MppApplication {
 
     public static void main(String[] args) {
         ArrayList<String>
-                stringList = new ArrayList<>(
-                Arrays
-                        .asList("sujan", "ram", "hari", "hari", "sujan"));
-        Number1.removeDuplicates(stringList);
+                stringList = new ArrayList<>(Arrays.asList("sujan", "ram", "hari", "hari", "sujan"));
+        Mpp.removeDuplicates(stringList);
 
 
         ArrayList<Integer>
-                numberList = new ArrayList<>(
-                Arrays
-                        .asList(1, 10, 1, 2, 2, 3, 3, 10, 3, 4, 5, 5));
-        Number1.removeDuplicates(numberList);
+                numberList = new ArrayList<>(Arrays.asList(1, 10, 1, 2, 2, 3, 10, 3, 4, 5, 5));
+        Mpp.removeDuplicates(numberList);
+
+        int[] testArray = new int[]{5, 1};
+        System.out.println("--- Test for sum result :" + Mpp.testForSum(testArray, 88));
         SpringApplication.run(MppApplication.class, args);
     }
 
